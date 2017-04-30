7fdc3fb72ac23421fb30fe32e569603b8b7d2409
SirusDoma <com@cxo2.me>
2017-04-30 11:40:07 +0700

    Removed get prefix from DashboardController method

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    public function getIndex(Request $request)
+    public function index(Request $request)
     {
         // Ambil data statistik realisasi dari service statistic
         $service = resolve('Services\Statistic');
