3b004bf6f1e3158e8b515eb2208858a988e2d6d1
SirusDoma <com@cxo2.me>
2017-04-30 13:46:16 +0700

    Changed ActivityController to ResourceController

--- app/Http/Controllers/ActivityController.php
+++ app/Http/Controllers/ActivityController.php
@@ -6,8 +6,45 @@ use Illuminate\Http\Request;
 
 class ActivityController extends Controller
 {
-    public function index(Request $request)
+    public function index()
     {
+        //
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show(Request $request, $id)
+    {
+        // Untuk sementara, data program dan kegiatan ditempatkan di service.
+        $service = resolve('Services\Activity');
+        $data    = $service->getProgramData();
         
+        return View('pages.activity.show', [
+            'program'  => $data[0],
+            'activity' => $data[0]['activities'][0]
+        ]);
+    }
+
+    public function edit($id)
+    {
+        //
+    }
+
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    public function destroy($id)
+    {
+        //
     }
 }
